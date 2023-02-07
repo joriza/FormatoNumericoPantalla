9a0b0bbd81d2777984e32e860bf450dbd2646398
Jorge Javier Izaguirre <jizaguirre@huenei.com>
2023-02-07 13:58:24 -0300

    230207-1358

--- FormatoNumericoPantalla/Program.cs
+++ FormatoNumericoPantalla/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double miDoble = 123456.7890123;
+            int miInt = 637;
+
+            Console.WriteLine("Impresiones normales");
+            Console.WriteLine("El entero {0}", miInt);
+            Console.WriteLine("El doble {0}", miDoble);
+            Console.WriteLine("-----------");
+            Console.WriteLine("Impresiones con formato");
+            Console.WriteLine("Moneda {0:C}", miDoble);
+            Console.WriteLine($"Moneda {miDoble:C}"); // Como la linea anterior pero con fstring.
+
+            Console.ReadKey();
+
         }
     }
 }
