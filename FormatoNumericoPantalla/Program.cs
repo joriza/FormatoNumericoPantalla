2bb17edccb7ae760fa3f014c212fa7a105dadb10
Jorge Javier Izaguirre <jizaguirre@huenei.com>
2023-02-07 15:34:37 -0300

    230207-1534

--- FormatoNumericoPantalla/Program.cs
+++ FormatoNumericoPantalla/Program.cs
@@ -14,9 +14,14 @@
             Console.WriteLine("Impresiones con formato");
             Console.WriteLine("Moneda {0:C}", miDoble);
             Console.WriteLine($"Moneda {miDoble:C}"); // Como la linea anterior pero con fstring.
+            Console.WriteLine("Rellena lugares {0:D5}", miInt);
+            Console.WriteLine("Exponente {0:E}", miDoble);
+            Console.WriteLine("Lugares decimales {0:F2}", miDoble);
+
+
 
             Console.ReadKey();
-
+            
         }
     }
 }
